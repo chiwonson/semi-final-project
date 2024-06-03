226aa5bdbc40049ed556474def96dfb2516e17b4
kosmo <kosmo@DESKTOP-5V04VNB>
2024-06-03 15:10:23 +0900

    ghy

--- semi-project/src/a/b/c/common/CommonUtils.java
+++ semi-project/src/a/b/c/common/CommonUtils.java
@@ -2,4 +2,26 @@ package a.b.c.common;
 
 public class CommonUtils {
 
+	// MEMBER
+	public static final String MEM_IMG_UPLOAD_PATH = "C:\\00.KOSMO142\\30.web\\el_kos_semi_work\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\kosSpring\\fileupload\\mem";
+	public static final int MEM_IMG_FILE_SIZE = 10*1024*1024; // 10MB
+	public static final String MEM_EN_CODE = "UTF-8";
+		
+		
+	// BOARD
+	public static final String BOARD_IMG_UPLOAD_PATH = "C:\\00.KOSMO142\\30.web\\el_kos_semi_work\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\kosSpring\\fileupload\\board";
+	public static final int BOARD_IMG_FILE_SIZE = 10*1024*1024; // 10MB
+	public static final String BOARD_EN_CODE = "UTF-8";
+		
+	// BOARD 페이징  
+	public static final int BOARD_PAGE_SIZE = 3;
+	public static final int BOARD_GROUP_SIZE = 3;
+	public static final int BOARD_CUR_PAGE = 1;
+	public static final int BOARD_TOTAL_COUNT = 0;
+		
+	// 상품 
+	public static final String PRODUCT_IMG_UPLOAD_PATH = "C:\\00.KOSMO142\\30.web\\el_kos_semi_work\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\kosSpring\\fileupload\\product";
+	public static final int PRODUCT_IMG_FILE_SIZE = 10*1024*1024; // 10MB
+	public static final String PRODUCT_EN_CODE = "UTF-8";
+		
 }
